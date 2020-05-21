7d8e1eb765d40c381917b9af1b490c9bc625c8c0
Edward Rodriguez <edwardR36@hotmail.com>
2020-05-21 07:28:45 -0400

    Change app function to a class Add onchange to searchbox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-  return (
-    <Fragment>
-      <header className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox />
-      </header>
-      <CardList robots={robots} />
-    </Fragment>
-  );
-};
+// STATE : description of app / object that describes application
+// ex: robot and whatever is entered in the searchbox
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: '',
+    };
+  }
+
+  onSearchChange(event) {
+    console.log(event);
+  }
+
+  render() {
+    return (
+      <div className='tc'>
+        <header>
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+        </header>
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
